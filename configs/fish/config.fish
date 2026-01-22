26255eb5dbea08e6cf50a82a1d25be65ad02bedf
Davyusow <dayvsonf27@gmail.com>
2026-01-22 02:18:02 -0300

    Atualização das configs do fish-shell

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,13 +1,22 @@
-set -g theme_nerd_fonts yes
-set -g theme_display_user yes
-set -g theme_powerline_fonts yes
-set -g theme_display_screen no
-
-set -g theme_color_user_bg '#ff8800'
-set -g theme_color_user '#ffffff'
-
-set -g theme_color_scheme gruvbox
+# Configurações do oh-my-fish -> Transitei para o fisher mas caso eu retorne as configs são estas
+# set -g theme_powerline_fonts yes
+# set -g theme_nerd_fonts yes
+# set -g theme_display_user ssh
+# set -g theme_display_screen no
+#
+# set -g theme_display_date no
+# set -g theme_display_sudo_user yes
+# set -g theme_color_scheme gruvbox
+# set -g theme_newline_cursor yes
 
 if status is-interactive
-
+    # Outros comandos para sessões interativas
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/davyusow/.ghcup/bin
+set -g fish_greeting ""
+
+alias ls="exa --icons"
+
+fish_add_path /home/davyusow/.spicetify
+pyenv init - fish | source
